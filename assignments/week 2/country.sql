2b6bc0d77b5223e190d4ddf8a4e976dbde1ed6a9
Zachary Estes <zachestes187@gmail.com>
2022-09-27 11:38:54 -0700

    country SQL excercise

---
+++ assignments/week 2/country.sql
@@ -0,0 +1,17 @@
+CREATE TABLE country(
+	countryName varchar(20),
+	population int,
+	abbreviations varchar(3),
+	continent varchar(20),
+	worldCupGroup varchar(20)
+)
+
+INSERT INTO country VALUES ('United States', 329500000, 'USA', 'North America', 'Group B');
+
+INSERT INTO country VALUES ('France', 67390000, 'FRA', 'Europe', 'Group D');
+
+INSERT INTO country VALUES ('Portugal', 10310000, 'PRT', 'Europe', 'Group H');
+
+INSERT INTO country VALUES ('Brazil', 212600000, 'BRA', 'South America', 'Group G');
+
+INSERT INTO country VALUES ('Germany', 83240000, 'GER', 'Europe', 'Group E');
